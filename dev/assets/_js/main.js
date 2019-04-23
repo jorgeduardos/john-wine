c5bab7f53841af44367c2d12bcb8b38ba776d02a
Jorge Sosa <jsosa@wearealloy.com>
2019-04-23 18:42:38 -0500

    first commit

---
+++ dev/assets/_js/main.js
@@ -0,0 +1,70 @@
+
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+import Foundation from 'foundation-sites';
+
+// import JS files here using ES6 import statement
+import slick from 'slick-carousel';
+import waypoint from '../../../node_modules/waypoints/lib/noframework.waypoints';
+
+
+$(document).foundation()
+
+
+var homeBkg = new TweenMax.fromTo("#bkg-after", 9,{background: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1))'},{ background: 'linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))', ease: Linear.easeNone });
+var logoHero = new TweenMax.to(".logo-hero", 1, { alpha: 1, ease: Linear.easeNone, delay: 9 });
+var arrowHero = new TweenMax.to("#home-arrow", 1, { alpha: 1, ease: Linear.easeNone, delay: 9.8 });
+
+// main
+
+var mainHeight = $('.chianti-container').height() + $('.landscape-container').height()*2;
+
+$('.about-main').height(mainHeight);
+
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: ".chianti-container",
+    duration: $('.chianti-container').height()/2,
+    offset: $('.chianti-container').height()/2
+})
+.setTween(".chianti-container", 1, {alpha: 0}) // trigger a TweenMax.to tween
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+var chianTextScene = new ScrollMagic.Scene({
+    triggerElement: ".chianti-container",
+    offset: $('.text-container-chianti').height()
+})
+.setClassToggle('.text-container-chianti', 'appear') // trigger a TweenMax.to tween
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({
+    triggerElement: ".chianti-container",
+    offset: $('.chianti-container').height()
+})
+.setTween(".landscape-container", 0.8, {alpha: 1}) // trigger a TweenMax.to tween
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+
+var landscapeTween = new TweenMax.to('.landscape-container', 1, {'background': 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(../../media/wine-landscape.jpg)'})
+var linearGrsdient = new ScrollMagic.Scene({
+    triggerElement: $('.chianti-container'),
+    offset: $('.chianti-container').height(),
+})
+.setTween(landscapeTween)
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+var landscapeTextScene = new ScrollMagic.Scene({
+    triggerElement: $('.chianti-container'),
+    offset: $('.chianti-container').height()
+})
+.setClassToggle(".text-container-landscape", 'appear') // trigger a TweenMax.to tween
+.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+    
---
+++ gulpfile.js
@@ -0,0 +1,191 @@
+'use strict';
+
+//**************************************
+
+// VARIABLES 
+
+  // Destination path variables.
+    
+  var pathToCms = {
+    js: 'cms/web/assets/js',
+    css: 'cms/web/assets/css',
+    media: 'cms/web/media',
+    fonts: 'cms/web/assets/fonts',
+    img: 'cms/web/assets/img',
+    html: 'cms/templates/',
+    indexHtml: './templates/index.html', // this path has to be in relation to the index.html file found in the cms folder
+    devFiles: 'cms/dev_files'
+  }
+
+  //global variables
+
+  var vHost = 'craft3.johnwine.test';
+
+//*************************************
+
+var gulp = require('gulp');
+const chmod = require('gulp-chmod');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var twig = require('gulp-twig');
+var browserSync = require('browser-sync').create();
+// var babel = require('gulp-babel');
+// var order = require("gulp-order");
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var imageminMozjpeg = require('imagemin-mozjpeg');
+var cache = require('gulp-cache');
+var mode = require('gulp-mode')({
+  modes: ["production", "development"],
+  default: "development",
+  verbose: false
+});
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+const webpack2 = require('webpack');
+const webpackStream = require('webpack-stream');
+const named = require('vinyl-named');
+// const webpackConfig = require('./webpack.config.js');
+
+//*************************************
+
+let webpackConfig = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+gulp.task('scripts', function() {
+  return gulp.src('dev/assets/_js/main.js')
+    .pipe(plumber())
+    .pipe(named())
+    .pipe(sourcemaps.init())
+      .pipe(webpackStream(webpackConfig, webpack2))
+      .pipe((mode.production(uglify())))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(pathToCms.js))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('copyScripts', function() {
+  return gulp.src('dev/assets/_js/scripts/**/*.js')
+  .pipe(gulp.dest(pathToCms.js + '/scripts'))
+})
+
+
+gulp.task('templates', function() {
+  return gulp.src('dev/templates/**/*.html') // run the Twig template parser on all .html files in the "src" directory
+      .pipe(twig())
+      .pipe(gulp.dest(pathToCms.html)) // output the rendered HTML files to the "dist" directory
+      .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('copyHTML', function(){
+  return gulp.src('dev/templates/**/*.html')
+  .pipe(gulp.dest(pathToCms.html))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+
+gulp.task('sass', function(){
+  return gulp.src('dev/assets/_scss/main.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+      .pipe(sass()) // Using gulp-sass
+      .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe((mode.production(cleanCSS())))
+    .pipe(gulp.dest(pathToCms.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('copyFonts', function(){
+  return gulp.src('dev/assets/fonts/**/*')
+    .pipe(gulp.dest(pathToCms.fonts))
+});
+
+gulp.task('img', () =>
+    gulp.src('dev/assets/img/**/*.+(png|jpg|gif|svg)')
+        .pipe((mode.production(imagemin([imageminMozjpeg(), imagemin.optipng()]))))
+        // .pipe(chmod(777))
+        .pipe(gulp.dest(pathToCms.img))
+        .pipe(browserSync.reload({
+        stream: true
+      }))
+);
+
+gulp.task('media', () =>
+    gulp.src('dev/media/**/*')
+        .pipe((mode.production(imagemin([imageminMozjpeg(), imagemin.optipng()]))))
+        // .pipe(chmod(777))
+        .pipe(gulp.dest(pathToCms.media))
+        .pipe(browserSync.reload({
+        stream: true
+      }))
+);
+
+gulp.task('clean', function(){
+ return del.sync(['cms/web/assets/', 'cms/web/media', 'cms/templates', 'cms/dev_files']);
+});
+
+gulp.task('cleanDev', function(){
+ return del.sync(['dev']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+      // baseDir: 'cms',
+      // index: pathToCms.indexHtml
+      proxy: vHost
+      // host: vHost
+  });
+});
+
+gulp.task('copyDevFiles', function(){
+  return gulp.src('dev/**/*')
+    .pipe(gulp.dest(pathToCms.devFiles))
+});
+
+
+gulp.task('copyCMSFiles', function(){
+  return gulp.src('cms/dev_files/**/*')
+    .pipe(gulp.dest('dev'))
+});
+
+
+// Taks to run on command line
+
+gulp.task('watch', ['clean', 'sass', 'scripts', 'copyScripts', 'img', 'media', 'copyFonts', 'copyHTML', 'browserSync'], function(){
+  gulp.watch('dev/assets/_scss/**/*.+(css|scss|sass)', ['sass']);
+  gulp.watch('dev/assets/_js/**/*.js', ['scripts']);
+  gulp.watch('dev/assets/img/**/*.+(png|jpg|gif|svg)', ['img']);
+  gulp.watch('dev/assets/fonts/**/*.+(eot|svg|ttf|woff)')
+  gulp.watch('dev/media/**/*.+(png|jpg|gif|svg)', ['media']);
+  gulp.watch('dev/templates/**/*.html', ['copyHTML']);
+});
+
+gulp.task('build', ['clean', 'sass', 'scripts', 'copyScripts', 'img', 'media', 'copyHTML', 'copyFonts', 'copyDevFiles']);
+
+gulp.task('buildDev', ['cleanDev', 'copyCMSFiles']);
+
+// http://analyticl.com/blog/frontend-templating-with-gulp-and-twig-js
